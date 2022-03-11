72d16b420f675ec3266caf9c99cd69564d487dd3
jsan <ahsan.wtc@gmail.com>
2022-03-11 16:43:17 +0500

    added contracts and set up compiler version

---
+++ contracts/ItemManager.sol
@@ -0,0 +1,45 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "./Item.sol";
+import "./Ownable.sol";
+
+contract ItemManager is Ownable {
+    enum SupplyChain { Created, Paid, Delivered }
+
+    struct S_Item {
+        SupplyChain _step;
+        string _identifier;
+        Item _item;
+    }
+
+    mapping(uint => S_Item) public items;
+    uint index;
+    
+    event SupplyChainStep(uint _itemIndex, uint _step, address _address);
+
+    function createItem(string memory _identifier, uint _priceInWei) public  OnlyOwner {
+        Item item = new Item(this, _priceInWei, index);
+        items[index]._item = item;
+        items[index]._step = SupplyChain.Created;
+        items[index]._identifier = _identifier;
+        emit SupplyChainStep(index, uint(SupplyChain.Created), address(item));
+        index++;
+    }
+
+    function triggerPayment(uint _index) public payable {
+        Item item = items[_index]._item;
+        require(address(item) == msg.sender, "Only items are allowed to update themselves");
+        require(item.priceInWei() == msg.value, "Not fully paid yet");
+        require(items[_index]._step == SupplyChain.Created, "Item is further in the chain");
+        items[_index]._step = SupplyChain.Paid;
+        emit SupplyChainStep(_index, uint(SupplyChain.Paid), address(item));
+    }
+
+    function triggerDelivery(uint _index) public OnlyOwner {
+        require(items[_index]._step == SupplyChain.Paid, "Item is further in the chain");
+        items[_index]._step = SupplyChain.Delivered;
+        emit SupplyChainStep(_index, uint(SupplyChain.Delivered), address(items[_index]._item));
+    }
+
+}
