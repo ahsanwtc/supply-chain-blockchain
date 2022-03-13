093f4ba2096651719045fa00834258888428dc00
jsan <ahsan.wtc@gmail.com>
2022-03-13 14:44:46 +0500

    transacting through ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Container, Typography, Box, CssBaseline, Divider, OutlinedInput, TextField, InputAdornment, Button, Alert, Stack } from '@mui/material';
 
-function App() {
+import { getWeb3, getContracts } from './utils';
+
+const App = () => {
+  const [web3, setWeb3] = useState(undefined);
+  const [contracts, setContracts] = useState(undefined);
+  const [accounts, setAccounts] = useState([]);
+  const [cost, setCost] = useState(0);
+  const [identifier, setIdentifier] = useState('');
+  const [error, setError] = useState(undefined);
+
+  useEffect(() => {
+    const init = async () => {
+      const web3 = await getWeb3();
+      const contracts = await getContracts(web3);
+      const accounts = await web3.eth.getAccounts();
+
+      setWeb3(web3);
+      setAccounts(accounts);
+      setContracts(contracts);
+    };
+
+    init();
+  }, []);
+
+  if (web3 === undefined || contracts === undefined || accounts.length === 0) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+
+  const handleButtonOnClick = async () => {
+    setError(undefined);
+    if (cost === 0 || identifier.length === 0) {
+      setError('Please provide valid information.');
+      return;
+    }
+
+    await contracts.itemManager.methods.createItem(identifier, cost).send({ from: accounts[0] });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+        <Typography variant="h4">Supply Chain Blockchain</Typography>
+        <Typography variant="subtitle1">A simple project to implement supply chain in blockchain.</Typography>
+        
+        <Divider variant="middle" />
+        
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', marginTop: '10px' }}>
+          <Typography variant="h6">Items</Typography>
+        </Box>
+        
+        <Divider variant="middle" />
+        
+        <Stack sx={{ width: '100%', marginTop: '10px' }} spacing={2}>
+          {error && <Alert severity="error">{error}</Alert>}
+        </Stack>
+
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', marginTop: '20px' }}>
+          <TextField
+            label="Cost in Wei"            
+            sx={{ m: 1, width: '25ch' }}
+            InputProps={{
+              startAdornment: <InputAdornment position="start">wei</InputAdornment>,
+            }}
+            onChange={e => setCost(e.target.value)}
+            value={cost}
+          />
+          <TextField
+            label="Item indentifier"            
+            sx={{ m: 1, width: '75ch' }}
+            InputProps={{
+              startAdornment: <InputAdornment position="start">identifier</InputAdornment>,
+            }}
+            onChange={e => setIdentifier(e.target.value)}
+            value={identifier}
+          />
+          <Button sx={{ m: 1, width: '25ch' }} variant="contained" onClick={handleButtonOnClick}>Create Item</Button>
+        </Box>
+
+      </Container>
+    </React.Fragment>
   );
 }
 
---
+++ client/src/utils.js
@@ -0,0 +1,30 @@
+import detectEthereumProvider from '@metamask/detect-provider';
+import Web3 from 'web3';
+
+import ItemManager from './contracts/ItemManager.json';
+import Item from './contracts/Item.json';
+
+export const getWeb3 = () => {
+  return new Promise(async (resolve, reject) => {
+    let provider = await detectEthereumProvider();    
+    if (provider) {
+      await provider.request({ method: 'eth_requestAccounts' });
+      try {
+        const web3 = new Web3(window.ethereum);
+        resolve(web3);
+      } catch(error) {
+        reject(error);
+      }
+    } 
+    
+    reject('Install Metamask');    
+  });
+};
+
+export const getContracts = async web3 => {
+  const networkId = await web3.eth.net.getId();
+  const itemManager = new web3.eth.Contract(ItemManager.abi, ItemManager.networks[networkId] && ItemManager.networks[networkId].address);
+  const item = new web3.eth.Contract(Item.abi, Item.networks[networkId] && Item.networks[networkId].address);
+
+  return { itemManager, item };
+};
